b564fd19db90e8555007276014cf93268afc83ac
Александр Макаров <makarov.alxr@yandex.ru>
2021-01-30 14:05:02 +0700

    add batch processing from file

--- domains.py
+++ domains.py
@@ -5,21 +5,44 @@ import json
 
 URL = 'http://217.79.62.70:8080/domains'
 ONLY_ENGLISH = '^[a-zA-Z]$'
+MAX_LENGTH = 1200
 
+
+def too_long_message():
+    click.echo(f'Too long sequence, max length {MAX_LENGTH}')
+    click.echo('Aborted')
+    exit(-1)
 
 @click.command()
-@click.option('--seq', help='Amino acid sequence to scan', prompt='Sequence')
+@click.option('--seq', help='Amino acid sequence to scan', default=None)
 @click.option('--raw', help='Get the raw output from server', is_flag=True)
 @click.option('--top', help='Print domains with highest score only',
               is_flag=True)
 @click.option('--output', help='File to write json output', default=None)
-def send(seq, raw, top, output):
-    if len(seq) > 10:
-        click.echo("Too long sequence, max length 1200")
-        click.echo("Aborted")
+@click.option('--file', help='Input file', default=None)
+@click.option('--sep', help='Separator in input file', default='\n')
+def send(seq, raw, top, output, file, sep):
+    if seq and file:
+        click.echo('Detected multiply sources, aborted')
         exit(-1)
+    elif not (seq or file):
+        seq = click.prompt('Enter a sequence', type=str)
 
+    if seq and len(seq) > MAX_LENGTH:
+        too_long_message()
+
+    if file:
+        with open(file, 'r') as fd:
+            seq = fd.read().split(sep)
+            seq = list(filter(lambda x: len(x), seq))  # filter empty seqs
+
+            for x in seq:
+                if len(x) > MAX_LENGTH:
+                    too_long_message()
+
+            seq = str(seq)
     seq = '\0' + seq + '\0'
+
     resp = requests.post(URL, seq).json()
 
     if raw:
@@ -35,6 +58,9 @@ def send(seq, raw, top, output):
                 json.dump(resp, file, indent=4)
         except Exception as err:
             click.echo(f"Can't write to file {output} because of {err}")
+
+    if not (raw or top or output):
+        click.echo(resp)
     return resp
 
 
